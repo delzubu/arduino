af48298b0d513a8f3fbc994fcd140841d639c4e9
Balazs Misangyi <Balazs_Misangyi@epam.com>
2022-03-22 17:21:39 +0100

    client added

--- BLE_Motor_Control/BLE_Motor_Control.ino
+++ BLE_Motor_Control/BLE_Motor_Control.ino
@@ -1,20 +1,23 @@
+// Check BLE_Motor_Control_bb.png for schematics
+
+
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
 
-const int PIN_INPUT = 2;
-const int INPUT_RANGE[] = {1161, 4095};
-const int OUTPUT_RANGE[] = {180, 0};
+const int PIN_INPUT = 2; // A0 / Analog IO 02 port as indicated on board
+const int INPUT_RANGE[] = {1161, 4095}; // Measured min and max input values
+const int OUTPUT_RANGE[] = {180, 0}; // 0-180
 
 #define SERVICE_UUID        "96c27a9e-a4bf-443b-b65c-fb68dce1d99a"
-#define CHARACTERISTIC_UUID "96c27a9e-a4bf-443b-b65c-fb68dce1d99a"
+#define CHARACTERISTIC_UUID "f38effdc-6191-4668-96b7-f0c2f47f32fb"
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
+int value = 0;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -35,13 +38,13 @@ void setup() {
   pServer->setCallbacks(new MyServerCallbacks());
   
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ   |
-                                         BLECharacteristic::PROPERTY_WRITE  |
-                                         BLECharacteristic::PROPERTY_NOTIFY |
-                                         BLECharacteristic::PROPERTY_INDICATE
-                                       );
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
 
   pCharacteristic->addDescriptor(new BLE2902());
   pService->start();
@@ -62,7 +65,8 @@ void loop() {
     if (deviceConnected) {
         int analogValue = analogRead(PIN_INPUT);
         int mappedValue = map(analogValue, INPUT_RANGE[0], INPUT_RANGE[1], OUTPUT_RANGE[0], OUTPUT_RANGE[1]);
-        pCharacteristic->setValue((uint8_t*)&mappedValue, 4);
+        Serial.print("Value:"); Serial.println(value);
+        pCharacteristic->setValue((uint8_t*)&value, sizeof(value));
         pCharacteristic->notify();
         value++;
         delay(10); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
